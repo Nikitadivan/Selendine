499abe02359c0412c118b99b828afde893fe0070
Nikitadivan <sarapul-mebel@mail.ru>
2022-05-22 21:28:48 +0400

    second commit

--- src/test/java/ru/netology/web/CardFormTest.java
+++ src/test/java/ru/netology/web/CardFormTest.java
@@ -16,11 +16,12 @@ public class CardFormTest {
         Configuration.holdBrowserOpen = true;
         open("http://localhost:9999");
         $("[data-test-id=city] input").setValue("Москва");
-        $("[data-test-id=date] input").setValue("29.05.2022");
+        $("[data-test-id=date] input").doubleClick();
+        $("[data-test-id=date] input").sendKeys("29.05.2022");
         $("[data-test-id=name] input").setValue("Никита");
         $("[data-test-id=phone] input").setValue("+79120050949");
         $("[data-test-id=agreement]").click();
         $(withText("Забронировать")).click();
-        $("[data-test-id=notification]").shouldHave(exactText("Успешно! Встреча успешно забронирована на 25.05.2022"), Duration.ofSeconds(15));
+        $("[data-test-id=notification]").shouldHave(exactText("Успешно! Встреча успешно забронирована на 29.05.2022"), Duration.ofSeconds(15));
     }
 }
